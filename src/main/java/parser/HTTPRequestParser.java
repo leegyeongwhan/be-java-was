e233794cb8fc5b1cea1c6b99d97c9ab77ecc51e1
leegyeongwhan <leekhy02@naver.com>
2023-04-05 19:34:34 +0900

    feat: getRequestURL기능은 HTTPRequest 클래스로 이동

--- src/main/java/parser/HTTPRequestParser.java
+++ src/main/java/parser/HTTPRequestParser.java
@@ -1,3 +1,5 @@
+
+
 package parser;
 
 import org.slf4j.Logger;
@@ -10,17 +12,13 @@ import java.io.IOException;
 public class HTTPRequestParser {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    public static String getRequestURL(String line) {
-        String[] token = line.split(" ");
-        String path = token[1];
-        log.debug("request path={}", path);
-        return path;
-    }
 
-    public static void parseRequestHeaders(String line, BufferedReader br) throws IOException {
+    public static String parseRequestHeaders(String line, BufferedReader br) throws IOException {
         while (!line.equals("") && !line.equals(null)) {
             log.debug("requestHeader={}", line);
             line = br.readLine();
+            return line;
         }
+        return "";
     }
 }
