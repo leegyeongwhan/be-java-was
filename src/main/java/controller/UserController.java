71edfcbce7f9cb1ba7e3112221ac2c4d39b94128
leegyeongwhan <leekhy02@naver.com>
2023-04-06 17:58:53 +0900

    feat: 유저 컨트롤러 구현 form 화면과 post 처리 요청을 받을시 index페이지로 넘어간다.

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,19 +1,35 @@
 package controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import request.HttpRequest;
 import response.HttpResponse;
+import response.HttpStatus;
+import webserver.RequestHandler;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+
 public class UserController extends FrontController {
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     @Override
     public void doPost(HttpRequest request, HttpResponse response) {
-        if (request.getUrl().startsWith("redirect:")) {
-            String url = request.getUrl().split("redirect:")[1];
-            response.response302Header(url);
+        log.debug("UserController doPost : {}", request, response);
+        response.sendRedirect("/index.html");
+    }
+
+    @Override
+    public void doGet(HttpRequest request, HttpResponse response) {
+        try {
+            log.debug("UserController doPost : {}", request, response);
+            byte[] body = Files.readAllBytes(new File("src/main/resources/templates" + request.getUrl()).toPath());
+            response.response200Header(body.length);
+            response.responseBody(body);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
