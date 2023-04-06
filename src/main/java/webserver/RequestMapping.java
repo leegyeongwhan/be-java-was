9116fcab50233209e15ca661f7d2cfa47bbff8f7
leegyeongwhan <leekhy02@naver.com>
2023-04-06 17:57:52 +0900

    feat: RequestMapping 은 HttpRequest의 url을 받아 맞는 컨트롤러를 호출해준다.

--- src/main/java/webserver/RequestMapping.java
+++ src/main/java/webserver/RequestMapping.java
@@ -1,25 +1,31 @@
 package webserver;
 
-import java.io.IOException;
+import controller.Controller;
+import controller.HomeController;
+import controller.UserController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 public class RequestMapping {
-
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
     private String url = "";
+    private static final ConcurrentHashMap<String, Controller> controllerMap = new ConcurrentHashMap<>();
 
-    public void mapping(String url) {
-        if (url.equals("/")) {
-            this.url = "/index.html";
-            return;
-        }
+    public RequestMapping() {
+        controllerMap.put("/index.html", new HomeController());
+        controllerMap.put("/user/form.html", new UserController());
+        controllerMap.put("/user/create", new UserController());
+    }
 
-        if (url.equals("/user/create")) {
-            this.url = "redirect:/";
-            return;
-        }
-        this.url = url;
+    public Controller mapping(String url) {
+        log.debug("url : {}", url);
+        return controllerMap.get(url);
     }
 
     public String getUrl() {
         return url;
     }
+
 }
