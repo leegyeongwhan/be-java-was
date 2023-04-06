58f3bde5d11e58086bac833415dda3b0670a1adf
leegyeongwhan <leekhy02@naver.com>
2023-04-07 02:47:07 +0900

    feat: Reponse 클래스에 정적인 파일들을 처리하는 코드를 추가합니다.

--- src/main/java/response/HttpResponse.java
+++ src/main/java/response/HttpResponse.java
@@ -2,18 +2,23 @@ package response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import request.HttpRequest;
 
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.Arrays;
 
 public class HttpResponse {
     //ToDO  응답 코드와 헤더를 먼저 설정. 이후에는 응답 바디를 만든다.
     private static final Logger log = LoggerFactory.getLogger(HttpResponse.class);
     private static DataOutputStream dos;
+    //TODO 사용해보자.
     private HttpStatus httpStatus;
-    private String responseHeader;
-    private String responseBody;
+    private String contentType;
+    private byte[] responseBody;
 
     public HttpResponse(OutputStream out) {
         this.dos = new DataOutputStream(out);
@@ -22,6 +27,7 @@ public class HttpResponse {
     public void sendRedirect(String url) {
         response302Header(url);
     }
+
     public void response302Header(String url) {
         try {
             dos.writeBytes("HTTP/1.1 302 Found \r\n");
@@ -34,7 +40,19 @@ public class HttpResponse {
     public void response200Header(int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + "text/html;charset=utf-8" + "\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    public void response200Header(int lengthOfBodyContent, String contentType) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: " + contentType + "\r\n");
+            log.debug("Content-Type: " + contentType + "\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
@@ -46,25 +64,75 @@ public class HttpResponse {
         try {
             dos.write(body, 0, body.length);
             dos.flush();
+            log.error("dos : {}", dos);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
-    }
-
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
     }
 
     public void setHttpStatus(HttpStatus httpStatus) {
         this.httpStatus = httpStatus;
     }
 
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public byte[] getResponseBody() {
+        return responseBody;
+    }
+
+    public void setResponseBody(byte[] responseBody) {
+        this.responseBody = responseBody;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
     @Override
     public String toString() {
         return "HttpResponse{" +
                 "httpStatus=" + httpStatus +
-                ", responseHeader='" + responseHeader + '\'' +
-                ", responseBody='" + responseBody + '\'' +
+                ", contentType='" + contentType + '\'' +
+                ", responseBody=" + Arrays.toString(responseBody) +
                 '}';
+    }
+
+    public void handleStaticResource(HttpRequest request, HttpResponse response) throws IOException {
+        String resourcePath = request.getUrl(); // 앞에 '/' 제거
+        log.debug("resourcePath : {}", resourcePath);
+
+        // 요청한 파일이 존재하는지 확인
+        File file = new File("src/main/resources/static" + resourcePath);
+
+        if (!file.exists()) {
+            response.setHttpStatus(HttpStatus.BAD_REQUEST);
+            return;
+        }
+
+        //TODO  차후 request 에서 컨텐트 타입을 만든다. 파일 타입 확인
+        String contentType;
+        if (resourcePath.startsWith("/html")) {
+            contentType = "text/html;charset=utf-8";
+        } else if (resourcePath.startsWith("/css")) {
+            contentType = "text/css";
+        } else if (resourcePath.startsWith("/js")) {
+            contentType = "application/javascript";
+        } else {
+            contentType = "application/octet-stream";
+        }
+
+        // 파일 읽기
+        byte[] body = Files.readAllBytes(new File("src/main/resources/static" + resourcePath).toPath());
+
+        log.debug("resourcePath파일읽은 후 : {}", resourcePath);
+
+        // HTTP 응답 전송
+        response.setHttpStatus(HttpStatus.OK);
+        response.setContentType(contentType);
+        log.debug("HTTP 응답 전송 : {}", response);
+        response.response200Header(body.length, contentType);
+        response.responseBody(body);
     }
 }
