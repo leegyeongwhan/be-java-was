1c387b4e3891032954eb888223b41270b641cac1
leegyeongwhan <leekhy02@naver.com>
2023-04-06 17:56:04 +0900

    feat: 로그 추가

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,17 +1,23 @@
 package controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import request.HttpRequest;
 import response.HttpResponse;
+import webserver.RequestHandler;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
 public class HomeController extends FrontController {
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     @Override
     public void doGet(HttpRequest request, HttpResponse response) {
         try {
+            log.debug("HomeController request : {}", request);
+            log.debug("HomeController response : {}", response);
             byte[] body = Files.readAllBytes(new File("src/main/resources/templates" + request.getUrl()).toPath());
             response.response200Header(body.length);
             response.responseBody(body);
