c3a77d2605c58b140ac33e87adf0088d9a807b06
leegyeongwhan <leekhy02@naver.com>
2023-04-06 17:59:20 +0900

    feat: 상태메세지 부분과 리스폰스 부분을 분리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
-
 package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.file.Files;
 
+import controller.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import request.HttpRequest;
@@ -33,49 +32,13 @@ public class RequestHandler implements Runnable {
             HttpResponse httpResponse = new HttpResponse(out);
 
             //TODO 스프링에서는 requestMapping을 통해 맞는 컨트롤러 메서드를 실행해준다 requestMapping을 구현해본다.
-            //ToDO 현재 HttpRequest에서 url을 정한다 redirect나 "/" 같은 상황에 대처하며 경로를 편하게 하기위해 requestMapping을 구현한다.
-            requestMapping.mapping(httpRequest.getUrl());
-            getView(out, requestMapping.getUrl());
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+            //ToDO 현재 HttpRequest에서 url을 정한다 redirect나 "/" 같은 상황에 대처하며 경로를 편하게 하기위해 requestMapping을 구현한다.\
+            log.debug("RequestHandler  HttpRequest : {}", httpRequest);
+            Controller controller = requestMapping.mapping(httpRequest.getUrl());
+            log.debug("controller : {}", controller);
 
-    private void getView(OutputStream out, String url) throws IOException {
-        DataOutputStream dos = new DataOutputStream(out);
-        if (url.startsWith("redirect:")) {
-            response302Header(dos, url);
-            return;
-        }
-        byte[] body = Files.readAllBytes(new File("src/main/resources/templates" + url).toPath());
-        response200Header(dos, body.length);
-        responseBody(dos, body);
-    }
-
-    private void response302Header(DataOutputStream dos, String view) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Location: " + view.split("redirect:")[1] + "\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            controller.service(httpRequest, httpResponse);
+//            getView(out, requestMapping.getUrl());
         } catch (IOException e) {
             log.error(e.getMessage());
         }
