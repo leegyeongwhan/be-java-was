039be3e61bfe19fc62336a5a21f2c5ce7990345e
leegyeongwhan <leekhy02@naver.com>
2023-04-08 04:10:36 +0900

    feat: 기존 url을 파싱 하는 로직에서 새로운 경로들이 추가될때마다 분기를 해야하는 부분을 url하나로 통합 하기위해 코드변경

--- src/main/java/request/HttpRequest.java
+++ src/main/java/request/HttpRequest.java
@@ -1,17 +1,15 @@
 package request;
 
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.RequestHandler;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URLDecoder;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Map;
+
+import static request.ContentType.*;
 
 
 public class HttpRequest {
@@ -20,8 +18,10 @@ public class HttpRequest {
     //start line
     private String method;
     private String url;
+    private String version;
 
-    private Map<String, String> requestHeader;
+    private ContentType contentType;
+    private Map<String, String> requestData;
     private String requestBody;
 
     public HttpRequest(InputStream in) throws IOException {
@@ -40,6 +40,7 @@ public class HttpRequest {
         //TODO startLine, header ,body 으로 구분한다.
         //http 메서드로 구분
         if (readLine.startsWith("GET") || readLine.startsWith("POST")) {
+            log.debug("get or post = {} ", readLine);
             readStartLine(readLine);
         }
         //TODO Header 부분을 분리한다.
@@ -47,33 +48,14 @@ public class HttpRequest {
             readHeaderLine(readLine);
         }
         //TODO Body 부분을 분리한다.
+
+//        parseStartLine(readLine);
+//        parseHeader(readLine);
     }
 
     private void readHeaderLine(String readLine) {
         //TODO header부분을 파싱한다.
         //requestHeader.put(headerName, value);
-    }
-
-    private void readStartLine(String readLine) {
-        String[] tokens = readLine.split(" ");
-        this.method = tokens[0];
-        String url = tokens[1];
-        log.debug("readStartLine  method : {}", method);
-
-        if (url.startsWith("/user/create?")) {
-            this.url = url.split("\\?")[0];
-            String queryString = url.split("\\?")[1];
-
-            queryString = URLDecoder.decode(queryString, StandardCharsets.UTF_8);
-            log.debug("queryString : {}", queryString);
-
-            User user = User.createUser(queryString);
-            log.debug("user : {}", user);
-            log.debug("url : {}", this.url);
-            return;
-        }
-        log.debug("readStartLine url : {}", url);
-        this.url = url;
     }
 
     public String getMethod() {
@@ -84,13 +66,31 @@ public class HttpRequest {
         return url;
     }
 
-    @Override
-    public String toString() {
-        return "HttpRequest{" +
-                "method='" + method + '\'' +
-                ", url='" + url + '\'' +
-                ", requestHeader='" + requestHeader + '\'' +
-                ", requestBody='" + requestBody + '\'' +
-                '}';
+    public String getRequestBody() {
+        return requestBody;
+    }
+
+    private void readStartLine(String readLine) {
+        String[] tokens = readLine.split(" ");
+        this.method = tokens[0];
+        this.url = tokens[1].split("\\?")[0];
+        this.version = tokens[2];
+        this.contentType = of(tokens[1].split("\\?")[0]);
+
+        log.debug("tokens : {}", Arrays.toString(tokens));
+        log.debug("contentType : {}", contentType);
+
+        if (tokens[1].contains("?")) {
+            requestData.put("parameter", tokens[1].split("\\?")[1]); // name=learner
+            log.debug("requestData : {}", requestData);
+        }
+    }
+
+    public String getTypeDirectory() {
+        return contentType.getTypeDirectory();
+    }
+
+    public String getContentTypeHeader() {
+        return contentType.getContentTypeHeader();
     }
 }
