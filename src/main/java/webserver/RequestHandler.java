1f1ae10f2f264eba30c490a6d10964abf2a04074
leegyeongwhan <leekhy02@naver.com>
2023-04-08 04:13:02 +0900

    feat: 기존 정적인 경로들이 오면 따로 핸들러를 구현해 처리하려 시도 했지만 불편함과 어려움을 느껴 컨트롤러로 처리 하도록 변경

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -14,6 +14,7 @@ public class RequestHandler implements Runnable {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
     private Socket connection;
     private RequestMapping requestMapping = new RequestMapping();
+    //rivate handleStaticResource staticResource = new handleStaticResource();
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -26,35 +27,28 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다. 구현로직만 있는 유틸성 클래스를 구현한다.
-
+            //halder
             HttpRequest httpRequest = new HttpRequest(in);
 
             //TODO HttpResponse로 out을 받는것 이 맞을까?
             HttpResponse httpResponse = new HttpResponse(out);
-
+            //static한 경로들은 따로 핸들링하고  get css/머시꺵이/
+            //log.debug("Http static URL {}", httpRequest.getStaticUrl());
             //TODO 스프링에서는 requestMapping을 통해 맞는 컨트롤러 메서드를 실행해준다 requestMapping을 구현해본다.
             //ToDO 현재 HttpRequest에서 url을 정한다 redirect나 "/" 같은 상황에 대처하며 경로를 편하게 하기위해 requestMapping을 구현한다.\
-            log.debug("RequestHandler  HttpRequest : {}", httpRequest);
 
-            //다음 url을 기준으로 정적파일을 만들지 동적을 만들지 구분한다.
-            if (httpRequest.getUrl().startsWith("/css") || httpRequest.getUrl().startsWith("/js") || httpRequest.getUrl().startsWith("/fonts")
-                    || httpRequest.getUrl().startsWith("/favicon.ico")) {
-                httpResponse.handleStaticResource(httpRequest, httpResponse);
-            } else {
-                Controller controller = requestMapping.mapping(httpRequest.getUrl());
+           // log.debug("httpRequest : {}", httpRequest.toString());
+            Controller controller = requestMapping.mapping(httpRequest.getUrl());
+            if (controller != null) {
                 log.debug("controller : {}", controller);
                 controller.service(httpRequest, httpResponse);
             }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
-
     }
-
 
     //TODO css/bootstrap.min.css   /css/styles.css /js/jquery-2.2.0.min.js  /js/scripts.js  /js/bootstrap.min.js  /favicon.ico
     // 다음과 같은 정적 파일들은 따로 랜더링 한다.
-
-
 }
 
