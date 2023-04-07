446b136b5eb4c981861c1c7e51fd9e0614379223
leegyeongwhan <leekhy02@naver.com>
2023-04-08 04:15:35 +0900

    feat: 파비콘 컨트롤러 구현

---
+++ src/main/java/controller/ResourceController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import request.HttpRequest;
+import response.HttpResponse;
+import webserver.RequestHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class ResourceController extends FrontController {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    @Override
+    public void doGet(HttpRequest request, HttpResponse response) {
+        try {
+            //         log.debug("ResourceController request : {}", request);
+            log.debug("ResourceController request.getUrl() : {}", request.getUrl());
+            byte[] body = Files.readAllBytes(new File(request.getTypeDirectory() + request.getUrl()).toPath());
+            log.debug("ResourceController + request.getUrl()).toPath() {} :", request.getTypeDirectory() + request.getUrl());
+
+            response.response200Header(body.length, request.getContentTypeHeader());
+            response.responseBody(body);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
