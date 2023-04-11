b6550453b4a59b77d13de680e01a15a158184cf8
leegyeongwhan <leekhy02@naver.com>
2023-04-11 17:48:59 +0900

    refactor: satic 메서드로 인해 메서드명 변경

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -28,7 +28,7 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다. 구현로직만 있는 유틸성 클래스를 구현한다.
             //halder
-            HttpRequest httpRequest = new HttpRequest(in);
+            HttpRequest httpRequest = HttpRequest.of(in);
 
             //TODO HttpResponse로 out을 받는것 이 맞을까?
             HttpResponse httpResponse = new HttpResponse(out);
@@ -37,7 +37,7 @@ public class RequestHandler implements Runnable {
             //TODO 스프링에서는 requestMapping을 통해 맞는 컨트롤러 메서드를 실행해준다 requestMapping을 구현해본다.
             //ToDO 현재 HttpRequest에서 url을 정한다 redirect나 "/" 같은 상황에 대처하며 경로를 편하게 하기위해 requestMapping을 구현한다
 
-           // log.debug("httpRequest : {}", httpRequest.toString());
+            // log.debug("httpRequest : {}", httpRequest.toString());
             Controller controller = requestMapping.mapping(httpRequest.getUrl());
             if (controller != null) {
                 log.debug("controller : {}", controller);
