8bc6c31160f146156c608047938487d5fcf511bd
leegyeongwhan <leekhy02@naver.com>
2023-04-11 17:45:15 +0900

    feat: body는 contentLength를 header로 부터 받아 초기화합니다.

--- src/main/java/request/HttpRequestBody.java
+++ src/main/java/request/HttpRequestBody.java
@@ -1,4 +1,40 @@
 package request;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HttpRequestBody {
+    private Map<String, String> body;
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    public HttpRequestBody(Map<String, String> body) {
+        this.body = body;
+    }
+
+    public static HttpRequestBody of(BufferedReader br, int contentLength) throws IOException {
+        String queryString = readData(br, contentLength);
+        Map<String, String> parsingQueryMap = ParsingQueryString(queryString);
+        log.debug(" HttpRequestBody contentLength : {}", contentLength);
+        return new HttpRequestBody(parsingQueryMap);
+    }
+
+    public static String readData(BufferedReader br, int contentLength) throws IOException {
+        char[] body = new char[contentLength];
+        br.read(body, 0, contentLength);
+        return String.copyValueOf(body);
+    }
+
+    public static Map<String, String> ParsingQueryString(String queryString) {
+        String[] tokens = queryString.split("&");
+        Map<String, String> body = new HashMap<>();
+        log.debug(" HttpRequestBody queryString : {}", queryString);
+
+        return body;
+    }
 }
