7ad5b4f7e05806363853c44a741a6f1482916813
leegyeongwhan <leekhy02@naver.com>
2023-04-11 17:44:22 +0900

    feat: request를 정적 팩토리 메서드를 사용해 머리,시작부분, 바디를 나눕니다.
    각자의 객체가 최기화 하도록 하기위해 분리했습니다.

--- src/main/java/request/HttpRequest.java
+++ src/main/java/request/HttpRequest.java
@@ -7,94 +7,58 @@ import webserver.RequestHandler;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
-import static request.ContentType.*;
-
 
 public class HttpRequest {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    //start line
-    private HttpMethod method;
-    private String url;
-    private String version;
-
-    private ContentType contentType;
-    private final Map<String, String> requestData = new HashMap<>();
+    private HttpRequestStartLine httpRequestStartLine;
+    //header
     private HttpRequestHeader httpRequestHeader;
+    //body
     private HttpRequestBody httpRequestBody;
 
-    public HttpRequest(InputStream in) throws IOException {
+    //TODO 정적 팩토리 메서드(Static Factory Method)
+    public static HttpRequest of(InputStream in) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-        String line;
-        while (!(line = br.readLine()).equals("")) {
-            if (line == null) {
-                return;
-            }
-            // log.debug("line : {}", line);
-            readRequest(line);
-        }
-    }
-
-    private void readRequest(String readLine) {
         //TODO startLine, header ,body 으로 구분한다.
-        //http 메서드로 구분
+        HttpRequestStartLine requestLine = HttpRequestStartLine.of(br);
+        HttpRequestHeader requestHeader = HttpRequestHeader.of(br);
+        HttpRequestBody requestBody = HttpRequestBody.of(br, requestHeader.getContentLength());
 
-        String[] tokens = readLine.split(" ");
-        log.debug("tokens : {}", Arrays.toString(tokens));
-
-        if (readLine.startsWith("GET") || readLine.startsWith("POST")) {
-            log.debug("get or post = {} ", readLine);
-            readStartLine(tokens);
-        }
-        //TODO Header 부분을 분리한다.
-        if (readLine.contains(": ")) {
-            readHeaderLine(readLine);
-        }
-        //TODO Body 부분을 분리한다.
-
+        return new HttpRequest(requestLine, requestHeader, requestBody);
     }
 
-    /**
-     * @param HttpRequest StartLine 분리 메서드     메서드 ,  url , 버전 으로분리
-     * @return
-     */
-    private void readStartLine(String[] tokens) {
-        //request 에서 startLine 분리한다
-        this.method = HttpMethod.valueOf(tokens[0]);
-        this.url = tokens[1].split("\\?")[0];
-        this.version = tokens[2];
-        this.contentType = of(tokens[1].split("\\?")[0]);
-        log.debug("contentType : {}", contentType);
-        //쿼리파라미터로 데이터를 넘길경우
-        if (tokens[1].contains("?")) {
-            requestData.put("parameter", tokens[1].split("\\?")[1]); // name=learner
-            log.debug("requestData : {}", requestData);
-        }
+    public HttpRequest(HttpRequestStartLine httpRequestStartLine, HttpRequestHeader httpRequestHeader, HttpRequestBody httpRequestBody) {
+        this.httpRequestStartLine = httpRequestStartLine;
+        this.httpRequestHeader = httpRequestHeader;
+        this.httpRequestBody = httpRequestBody;
     }
 
-    private void readHeaderLine(String line) {
-        //TODO header부분을 파싱한다.
-        //requestHeader.put(headerName, value);
+    public HttpRequestStartLine getHttpRequestStartLine() {
+        return httpRequestStartLine;
     }
 
-    public HttpMethod getMethod() {
-        return method;
+    public HttpRequestHeader getHttpRequestHeader() {
+        return httpRequestHeader;
+    }
+
+    public HttpRequestBody getHttpRequestBody() {
+        return httpRequestBody;
     }
 
     public String getUrl() {
-        return url;
+        return httpRequestStartLine.getUrl();
     }
 
-
     public String getTypeDirectory() {
-        return contentType.getTypeDirectory();
+        return getHttpRequestHeader().getContentType().getTypeDirectory();
     }
 
     public String getContentTypeHeader() {
-        return contentType.getContentTypeHeader();
+        return getHttpRequestHeader().getContentType().getContentTypeHeader();
+    }
+
+    public HttpMethod getMethod() {
+        return httpRequestStartLine.getMethod();
     }
 }
