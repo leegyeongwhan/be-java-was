0ac7bfe14c08119d19861ceb2be648fe59f671e6
leegyeongwhan <leekhy02@naver.com>
2023-04-16 20:27:42 +0900

    feat: 리플랙션 클래스를 초기화하는 메서드를 WebServer 시작단계에 실행합니다.

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -11,6 +11,8 @@ public class WebServer {
     private static final int DEFAULT_PORT = 8080;
 
     public static void main(String args[]) throws Exception {
+        RequestMapper.run();
+
         int port = 0;
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
