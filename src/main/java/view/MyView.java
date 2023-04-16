ba1cd66b5ed7ab14f3455996483045c869569970
leegyeongwhan <leekhy02@naver.com>
2023-04-17 04:32:45 +0900

    feat: 디스페처 서블릿에서 전달받은 경로를 바탕으로 1.리다이렉트 하는경우 2.200ok 성공 두가지 케이스로 분리해서 response를 만듭니다.

--- src/main/java/view/MyView.java
+++ src/main/java/view/MyView.java
@@ -1,26 +1,71 @@
 package view;
 
+import http.request.ContentType;
+import http.response.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 import webserver.RequestHandler;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 public class MyView {
     private String viewPath;
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private ContentType contentsType;
+    private HttpRequest request;
 
-    public MyView(String path) {
-        this.viewPath = path;
+    public MyView(String viewName, HttpRequest request) {
+        this.viewPath = viewName;
+        this.request = request;
     }
 
-    public void render(HttpRequest request, HttpResponse response) throws IOException {
-        byte[] body = Files.readAllBytes(new File(request.getTypeDirectory() + request.getUrl()).toPath());
-        response.response200Header(body.length, request.getContentTypeHeader());
-        response.responseBody(body);
+    //TODO 리스폰스가 render을 할까 view가 render을 할까.
+    public void viewResolver(HttpRequest request, HttpResponse response) throws IOException {
+        //리다이렉트 하는경우
+        response.setHttpVersion("HTTP/1.1");
+
+        if (viewPath.contains("redirect:")) {
+            String viewName = viewPath.replace("redirect:", "");
+            MyView view = new MyView(viewName, request);
+            log.debug("Location viewName: {} ", viewName);
+
+            response.setStatus(HttpStatus.FOUND)
+                    .addHeader("Location", viewName)
+                    .setView(view);
+
+            response.sendRedirect(viewName);
+        }
+        //HTTP/1.1 200 OK
+        //Content-Type: text/html
+        //Content-Length: 1234
+        //body
+        //200성공 경우 컨텐트 타입이있다
+        MyView view = new MyView(viewPath, request);
+
+        response.setStatus(HttpStatus.OK)
+                .addHeader("Content-Type", request.getTypeDirectory())
+                .setView(view);
+
+        response.render();
+    }
+
+    public boolean validBody() {
+        return contentsType != null ? true : false;
+    }
+//    public void render(HttpRequest request, HttpResponse response) throws IOException {
+//        log.debug("path : {} ", request.getTypeDirectory() + request.getUrl());
+//        byte[] body = Files.readAllBytes(new File(request.getTypeDirectory() + viewPath).toPath());
+//        response.response200Header(body.length, request.getContentTypeHeader());
+//        response.responseBody(body);
+//    }
+
+    public String getViewPath() {
+        return viewPath;
+    }
+
+    public HttpRequest getRequest() {
+        return request;
     }
 }
