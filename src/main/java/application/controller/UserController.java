22bd95e65bfbe86cef5e4f00b8f254fc87d9e1b3
leegyeongwhan <leekhy02@naver.com>
2023-04-17 04:33:44 +0900

    feat: 경로수정, 매개변수 변화 리플렉션 invoke의 변화로 컨트롤러 전체 매개변수 변경

--- src/main/java/application/controller/UserController.java
+++ src/main/java/application/controller/UserController.java
@@ -1,5 +1,8 @@
 package application.controller;
 
+import http.request.HttpRequest;
+import http.request.HttpRequestBody;
+import http.response.HttpResponse;
 import webserver.annotation.Controller;
 import webserver.annotation.RequestMapping;
 import application.db.Database;
@@ -13,19 +16,22 @@ import webserver.RequestHandler;
 import java.util.Map;
 
 @Controller
-public class UserController extends FrontController{
+public class UserController extends FrontController {
     private static final String INDEX_HTML = "/index.html";
     private static final String REDIRECT = "redirect:/";
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
     private final Database database = new Database();
 
     public ModelAndView process(Map<String, String> parameters) {
-        createUser(parameters);
         return new ModelAndView(REDIRECT + INDEX_HTML);
     }
 
     @RequestMapping(path = "/user/create", method = HttpMethod.POST)
-    private void createUser(Map<String, String> parameters) {
+    public String createUser(HttpRequest request, HttpResponse response) {
+        HttpRequestBody httpRequestBody = request.getHttpRequestBody();
+        Map<String, String> parameters = httpRequestBody.getBody();
+        log.debug("parameters = {}", parameters);
+
         String userId = parameters.get("userId");
         String password = parameters.get("password");
         String name = parameters.get("name");
@@ -34,5 +40,11 @@ public class UserController extends FrontController{
         User user = new User(userId, password, name, email);
         log.debug("생성한유저 = {}", user);
         database.addUser(user);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/user/form", method = HttpMethod.GET)
+    public String userForm(HttpRequest request, HttpResponse response) {
+        return "/user/form.html";
     }
 }
