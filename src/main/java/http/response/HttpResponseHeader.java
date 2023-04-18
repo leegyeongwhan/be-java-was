1101418946757ed39ef2e8d9ef7e066c6c98abe3
leegyeongwhan <leekhy02@naver.com>
2023-04-18 15:08:33 +0900

    feat: 리스폰스 헤더는 쿠키에 대한 정보를 가지고잇는다 리스폰스를 통해 쿠키를 다룬다.

--- src/main/java/http/response/HttpResponseHeader.java
+++ src/main/java/http/response/HttpResponseHeader.java
@@ -1,12 +1,34 @@
 package http.response;
 
+import cookie.Cookie;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HttpResponseHeader {
     private Map<String, String> headers = new HashMap<>();
+    private Cookie cookies;
 
     public void put(String key, String value) {
         headers.put(key, value);
+    }
+
+    public boolean isEmptyCookie() {
+        return this.cookies.isEmpty();
+    }
+
+    public void addCookie(String name, String value) {
+        this.cookies.addCookie(name, value);
+    }
+
+    public String parseCookie() {
+        return this.cookies.parse();
+    }
+
+    public void addResponseHeader(String key, String value) {
+        this.headers.put(key, value);
+    }
+
+    public void addMySessionCookie(Cookie mySessionCookie) {
+        this.cookies = mySessionCookie;
     }
 }
