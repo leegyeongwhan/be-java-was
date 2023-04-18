412790c4471bc14e9c155cb42cdf63efac4b98cc
leegyeongwhan <leekhy02@naver.com>
2023-04-18 15:08:09 +0900

    feat: 리다이렉트나 200 응답을 할때 header에 쿠키를 추가한다 쿠니는 세션을 가지고잇는다

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -1,6 +1,7 @@
 package http.response;
 
 import cookie.Cookie;
+import http.request.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import view.MyView;
@@ -8,8 +9,10 @@ import view.MyView;
 import java.io.*;
 import java.nio.file.Files;
 
-
 public class HttpResponse {
+    private static final String COOKIE_NAME_PATH = "Path";
+    private static final String COOKIE_VALUE_PATH = "/";
+
     //ToDO  응답 코드와 헤더를 먼저 설정. 이후에는 응답 바디를 만든다.
     private static final Logger log = LoggerFactory.getLogger(HttpResponse.class);
     private DataOutputStream dos;
@@ -19,12 +22,13 @@ public class HttpResponse {
     private MyView view;
     private HttpResponseHeader httpResponseHeader;
 
-    public HttpResponse(OutputStream out) {
+    public HttpResponse(OutputStream out, HttpRequest request) {
         this.dos = new DataOutputStream(out);
         this.httpResponseHeader = new HttpResponseHeader();
     }
 
     public void sendRedirect(String url) {
+        addCookieHeader();
         response302Header(url);
     }
 
@@ -56,7 +60,7 @@ public class HttpResponse {
             log.debug("Content-Lengthe: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: " + contentType + "\r\n");
-            dos.writeBytes("response200Header defalut Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("esponse200Header defalut Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
@@ -84,7 +88,23 @@ public class HttpResponse {
         return httpStatus;
     }
 
-    public void addCookie(Cookie mySessionCookie) {
+    public void addSessionCookie(Cookie mySessionCookie) {
+        httpResponseHeader.addMySessionCookie(mySessionCookie);
+    }
+
+    public void addCookie(String name, String value) {
+        this.httpResponseHeader.addCookie(name, value);
+    }
+
+    private void addCookieHeader() {
+        if (this.httpResponseHeader.isEmptyCookie()) {
+            return;
+        }
+
+        this.httpResponseHeader.addCookie(COOKIE_NAME_PATH, COOKIE_VALUE_PATH);
+        String parseCookie = this.httpResponseHeader.parseCookie();
+        log.debug("addCookieHeader : {}", parseCookie);
+        addHeader("Set-Cookie", parseCookie);
     }
 
     @Override
@@ -97,11 +117,9 @@ public class HttpResponse {
 
     public void render() throws IOException {
         byte[] body = Files.readAllBytes(new File(view.getRequest().getTypeDirectory() + view.getViewPath()).toPath());
+        // getResponseHeader();
         response200Header(body.length, view.getRequest().getContentTypeHeader());
-//        if (view.validBody()) {
-//            log.debug("view.getViewPath()).toPath(): " + view.getViewPath());
-//            responseBody(body);
-//        }
+        addCookieHeader();
         responseBody(body);
     }
 
