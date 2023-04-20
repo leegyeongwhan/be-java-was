556f8245b223afb8e59cf6c93a5b474fe60159a0
leeseungwoo <vugil@naver.com>
2023-04-21 01:15:42 +0900

    feat: viewesolver 클래스로 분리

--- src/main/java/view/ViewResolver.java
+++ src/main/java/view/ViewResolver.java
@@ -1,2 +1,37 @@
-package view;public class ViewResolver {
+package view;
+
+import http.request.ContentType;
+import http.response.HttpResponse;
+import http.response.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import webserver.RequestHandler;
+
+public class ViewResolver {
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    public static void run(String viewPath, HttpResponse response) {
+        //리다이렉트 하는경우
+        response.setHttpVersion("HTTP/1.1");
+
+        if (viewPath.contains("redirect:")) {
+            String viewName = viewPath.replace("redirect:", "");
+
+            response.setStatus(HttpStatus.FOUND)
+                    .addHeader("Location", viewName)
+                    .setView(viewName);
+
+            response.render();
+            return;
+        }
+
+        ContentType contentType = HttpRequestUtils.findContent(viewPath);
+        response.setStatus(HttpStatus.OK)
+                .addHeader("Content-Type", contentType.getContentTypeHeader())
+                .setContentType(contentType)
+                .setView(viewPath);
+
+        response.render();
+    }
 }
