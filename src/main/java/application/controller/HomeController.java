2d704916ddbe76ec2f405f3761c4c25941d69b7f
leeseungwoo <vugil@naver.com>
2023-04-21 04:41:02 +0900

    feat:  홈컨트롤러 쿠키안의 세션으로 로그인 회원인지 비로그인 회원인지 구별하는 코드 추가

--- src/main/java/application/controller/HomeController.java
+++ src/main/java/application/controller/HomeController.java
@@ -1,14 +1,19 @@
 package application.controller;
 
 
+import application.model.User;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
+import session.SessionManager;
+import util.HttpRequestUtils;
 import webserver.annotation.Controller;
 import webserver.annotation.RequestMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpMethod;
 import webserver.RequestHandler;
+
+import java.util.Optional;
 
 @Controller
 public class HomeController extends FrontController {
@@ -22,6 +27,17 @@ public class HomeController extends FrontController {
 
     @RequestMapping(path = "/", method = HttpMethod.GET)
     public String home(HttpRequest request, HttpResponse response) {
+        Optional<String> cookie = request.getCookie();
+        String session = cookie.orElseThrow();
+        //쿠키가 등록된 회원
+        if (!session.isEmpty()) {
+            String sessionId = HttpRequestUtils.parseSessionId(cookie.orElseThrow());
+            User user = SessionManager.getAttribute(sessionId);
+
+            //로그인한 회원인 경우
+            response.setModelAttribute("loginId", user.getUserId());
+        }
+        response.setModelAttribute("loginId", "비회원");
         return "/index.html";
     }
 }
