ff25e0fdb43ceecc5061d087c93477ea27c4966c
leeseungwoo <vugil@naver.com>
2023-04-21 08:42:17 +0900

    feat: Object 형식에 맞게 문법 수정

--- src/main/java/view/ModelAndView.java
+++ src/main/java/view/ModelAndView.java
@@ -2,18 +2,12 @@ package view;
 
 import application.model.User;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ModelAndView {
 
     private String view;
     private Map<String, Object> model = new HashMap<>();
-
-    public ModelAndView(String view) {
-        this.view = view;
-    }
 
     public String getView() {
         return view;
@@ -28,17 +22,14 @@ public class ModelAndView {
     }
 
     public void setModelAttribute(String listName, Collection<User> list) {
-        int count = 0;
-        for (User user : list) {
-            addObject("user userId" + count, user.getUserId());
-            addObject("user password" + count, user.getPassword());
-            addObject("user name" + count, user.getName());
-            addObject("user email" + count, user.getEmail());
-            count++;
-        }
+        model.put(listName, list);
     }
 
     public void setModelAttribute(String key, String value) {
         addObject(key, value);
+    }
+
+    public void setView(String view) {
+        this.view = view;
     }
 }
