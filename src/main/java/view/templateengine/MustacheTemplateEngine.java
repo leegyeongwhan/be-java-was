238841a8c0628c5adb568154fb73403b28263dca
leegyeongwhan <leekhy02@naver.com>
2023-05-05 14:09:31 +0900

    feat: 로그인 로그아웃시 문자열처리 추가

--- src/main/java/view/templateengine/MustacheTemplateEngine.java
+++ src/main/java/view/templateengine/MustacheTemplateEngine.java
@@ -24,12 +24,31 @@ public class MustacheTemplateEngine implements TemplateEngine {
         StringBuilder sb = new StringBuilder(new String(html));
         //index페이지일 경우 로그인 여부를 체크해서 logout, login 을 구분하여 화면을 렌더링한다.
         //세션있으면 loginId 있어야한다
+        // 로그인시 모델이 로그인 아이디를 추가해서 모델링시 확인한다.
+        int start = sb.indexOf(START_TAG);
+        int end = sb.indexOf(END_TAG, start + START_TAG.length());
+
+        Object loginId = modelAndView.getModelAttribute("loginId");
+        log.debug("loginID", loginId);
+        if (loginId != null) {
+            log.debug("handleLoginStatus loginId", loginId);
+            start = sb.indexOf("{{^logout}}") + "{{^logout}}".length();
+            end = sb.indexOf("{{/^logout}}", start);
+            //{{^logout}}"과 "{{/^logout}}"으로 둘러싸인 부분 문자열이 제거
+            sb.replace(start - "{{^logout}}".length(), end + "{{/^logout}}".length(), "");
+            sb = new StringBuilder(sb.toString().replace("{{#login}}", "").replace("{{/login}}", ""));
+        } else {
+            // "{{#login}}" 문자열 다음에 오는 문자열의 시작
+            start = sb.indexOf("{{#login}}") + "{{#login}}".length();
+            //"{{#login}}"과 "{{/login}}" 사이의 부분 문자열을 추출하기 위해 사용
+            end = sb.indexOf("{{/login}}", start);
+            sb.replace(start - "{{#login}}".length(), end + "{{/login}}".length(), "");
+            sb = new StringBuilder(sb.toString().replace("{{^logout}}", "").replace("{{/^logout}}", ""));
+        }
 
         Collection<User> userCollection = (Collection<User>) modelAndView.getModel().get("users");
         if (userCollection != null) {
             sb = handleUserListStatus(modelAndView, sb);
-        } else {
-            sb = handleLoginStatus(modelAndView, sb);
         }
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -62,40 +81,6 @@ public class MustacheTemplateEngine implements TemplateEngine {
         //파싱한 문자를 시작지점부터 끝 테그까지 대체한다.
         String usersHtmlFragment = userHtml.toString();
         sb.replace(start, end + END_TAG.length(), usersHtmlFragment);
-        return sb;
-    }
-
-    private StringBuilder handleLoginStatus(ModelAndView modelAndView, StringBuilder sb) throws IOException {
-        int start = sb.indexOf(START_TAG);
-        int end = sb.indexOf(END_TAG, start + START_TAG.length());
-
-        Object loginId = modelAndView.getModelAttribute("loginId");
-        boolean isLoggedIn = false;
-
-        if (loginId != null) {
-            log.debug("handleLoginStatus loginId", loginId);
-            isLoggedIn = true;
-        }
-
-        if (isLoggedIn) {
-            start = sb.indexOf("{{^logout}}") + "{{^logout}}".length();
-            end = sb.indexOf("{{/^logout}}", start);
-            //{{^logout}}"과 "{{/^logout}}"으로 둘러싸인 부분 문자열이 제거
-            sb.replace(start - "{{^logout}}".length(), end + "{{/^logout}}".length(), "");
-            sb = new StringBuilder(sb.toString().replace("{{#login}}", "").replace("{{/login}}", ""));
-
-        } else {
-            log.debug("handleLoginStatus isLoggedIn 로그인 상태 체크", isLoggedIn);
-            // "{{#login}}" 문자열 다음에 오는 문자열의 시작
-            start = sb.indexOf("{{#login}}") + "{{#login}}".length();
-            //"{{#login}}"과 "{{/login}}" 사이의 부분 문자열을 추출하기 위해 사용
-            end = sb.indexOf("{{/login}}", start);
-            sb.replace(start - "{{#login}}".length(), end + "{{/login}}".length(), "");
-            sb = new StringBuilder(sb.toString().replace("{{^logout}}", "").replace("{{/^logout}}", ""));
-        }
-        System.out.println("sb = " + sb);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        outputStream.write(sb.toString().getBytes());
         return sb;
     }
 }
